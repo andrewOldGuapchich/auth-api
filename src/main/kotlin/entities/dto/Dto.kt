d9799bac69ff41a0a76108db9135170bf33049e5
andrewOldGuapchich <i@andrewkononov.ru>
2025-03-12 22:35:33 +0300

    [1.0]: update dto

--- src/main/kotlin/entities/dto/Dto.kt
+++ src/main/kotlin/entities/dto/Dto.kt
@@ -19,14 +19,14 @@ data class AuthRequest(
 data class RegisterResponse (
     val login: String,
     val token: String,
-    override val message: RegisterResponseMessageCode,
+    override val message: Any,
     override val status: Number,
     override val messageTimestamp: LocalDateTime? = LocalDateTime.now()
 ) : Response(message, status, messageTimestamp)
 
 data class AuthResponse(
     val token: String,
-    override val message: AuthResponseMessageCode,
+    override val message: Any,
     override val status: Number,
     override val messageTimestamp: LocalDateTime? = LocalDateTime.now()
 ) : Response(message, status, messageTimestamp)
